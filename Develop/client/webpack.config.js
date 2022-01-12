2fb08b7e830f6311378b5d3be472b2e5f537802c
ettaho <etta0311031@gmail.com>
2022-01-12 22:25:31 +1100

    code added for sw.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "JATE",
+        name: "TextEditor",
         short_name: "JATE",
         description: "",
         background_color: '#225ca3',
@@ -65,7 +65,8 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             }
           }
         }
